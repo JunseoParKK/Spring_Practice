d4c8de342f2c745aa86af0df1222b39426811797
Junseo Park <wnstj444@naver.com>
2023-02-19 23:41:26 +0900

    first commit

--- src/test/java/com/core/core/AutoAppConfigTest.java
+++ src/test/java/com/core/core/AutoAppConfigTest.java
@@ -1,6 +1,7 @@
 package com.core.core;
 
 import com.core.core.member.Member;
+import com.core.core.member.MemberRepository;
 import com.core.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,8 @@ public class AutoAppConfigTest {
     void basciScan(){
         AnnotationConfigApplicationContext ac=new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
+        MemberRepository memberRepository=ac.getBean(MemberRepository.class);
+        System.out.println("memberRepository = " + memberRepository);
         MemberService memberService=ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
     }
